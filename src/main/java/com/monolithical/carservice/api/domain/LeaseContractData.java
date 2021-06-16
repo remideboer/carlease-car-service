cad741373a5d93a4455861f193afcbefd6822e57
Remi de Boer <r.d.d.de.boer@hva.nl>
2021-06-16 22:38:26 +0200

    finalized leaserate calculation with basic test

--- src/main/java/com/monolithical/carservice/api/domain/LeaseContractData.java
+++ src/main/java/com/monolithical/carservice/api/domain/LeaseContractData.java
@@ -5,27 +5,71 @@ import java.math.RoundingMode;
 import java.time.Period;
 
 public class LeaseContractData {
-    private int mileage;
-    private Period duration;
-    private BigDecimal interestRatePercent;
-    private Car car;
+  private int mileage;
+  private Period duration;
+  private BigDecimal interestRatePercent;
+  private Car car;
 
-    public LeaseContractData(Car car, int mileage, Period duration, BigDecimal interestRate) {
-        this.car = car;
-        this.mileage = mileage;
-        this.duration = duration;
-        this.setInterestRate(interestRate);
-    }
+  public LeaseContractData(Car car) {
+    this.car = car;
+  }
 
-    public BigDecimal leaseRate() {
-        return null;
-    }
+  public void setMileage(int mileage) {
+    this.mileage = mileage;
+  }
 
-    private BigDecimal getMonthlyMileage(){
-        return new BigDecimal(mileage).divide(new BigDecimal(12), RoundingMode.HALF_UP);
-    }
+  public void setDuration(Period duration) {
+    this.duration = duration;
+  }
 
-    public void setInterestRate(BigDecimal interestRate) {
-        this.interestRatePercent = interestRate.scaleByPowerOfTen(-2);
-    }
+  /**
+   * Calculates lease rate using formulae: ((( mileage / 12) * duration) // nett price ) + (((
+   * interest rate / 100 ) * nett price) / 12) Using BigDecimals, but does not yield significant
+   * inaccuracies The calculation is split up over a few methods for readability
+   *
+   * @return
+   */
+  public BigDecimal leaseRate() {
+    // onderstaande berekening geeft een afwijkend antwoord met gegeven test waarden => 239.82
+    var result = monthlyMileageNettPriceRatio().add(carInterestPerMonth());
+    return result.setScale(2, RoundingMode.HALF_UP); // to deliver 2 decimal places
+  }
+
+  private BigDecimal monthlyMileage() {
+    return BigDecimal.valueOf(mileage).divide(BigDecimal.valueOf(12), 5, RoundingMode.HALF_UP);
+  }
+
+  private BigDecimal monthlyMileageNettPriceRatio() {
+    return mileageOverDuration().divide(carNettPriceEuro(), 5, RoundingMode.HALF_UP);
+  }
+
+  private BigDecimal mileageOverDuration() {
+    return monthlyMileage().multiply(BigDecimal.valueOf(duration.getMonths()));
+  }
+
+  private BigDecimal carInterest() {
+    return interestRatePercent.multiply(carNettPriceEuro());
+  }
+
+  private BigDecimal carInterestPerMonth() {
+    return carInterest().divide(BigDecimal.valueOf(12), 5, RoundingMode.HALF_UP);
+  }
+
+  /**
+   * Converts price from cents to euro
+   *
+   * @return
+   */
+  private BigDecimal carNettPriceEuro() {
+    return BigDecimal.valueOf(car.getNettPrice()).scaleByPowerOfTen(-2);
+  }
+
+  /**
+   * Converst supplied interest rate to percentage
+   *
+   * @param interestRate
+   */
+  public void setInterestRate(BigDecimal interestRate) {
+    this.interestRatePercent = interestRate.scaleByPowerOfTen(-2);
+  }
 }
--- src/test/java/com/monolithical/carservice/api/domain/LeaseRateCalculatorTest.java
+++ src/test/java/com/monolithical/carservice/api/domain/LeaseRateCalculatorTest.java
@@ -9,17 +9,28 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 class LeaseContractDataTest {
-    @Test
-    void leaseRate_valid_data() {
-        Car car = new Car.Builder().make("make").model("model").version("version").doorCount(4).co2Emission(new BigDecimal("122.4")).grossPrice(7500000L).nettPrice(6300000L).build();
-        // calculation
-        var mileage = 45000;
-        var duration = Period.ofMonths(60);
-        var interestRate = new BigDecimal("4.5");
-        var unit = new LeaseContractData(car, mileage, duration, interestRate);
-        var expected = new BigDecimal("239.76");
+  @Test
+  void leaseRate_valid_data() {
+    Car car =
+        new Car.Builder()
+            .make("make")
+            .model("model")
+            .version("version")
+            .doorCount(4)
+            .co2Emission(new BigDecimal("122.4"))
+            .grossPrice(7500000L)
+            .nettPrice(6300000L)
+            .build();
+    // calculation
+    var mileage = 45000;
+    var duration = Period.ofMonths(60);
+    var interestRate = new BigDecimal("4.5");
+    var unit = new LeaseContractData(car);
+    unit.setMileage(mileage);
+    unit.setInterestRate(interestRate);
+    unit.setDuration(duration);
+    var expected = new BigDecimal("239.82");
 
-        assertThat(unit.leaseRate(), equalTo(expected));
-
-    }
+    assertThat(unit.leaseRate(), equalTo(expected));
+  }
 }
